1f0abda8309216923c953dbbf8a8f9ac1c5a3d62
Luis Gizirian <v-lugizi@microsoft.com>
2019-02-27 18:33:46 -0300

    ported changes + fixed the cross-repo paths under /adfs

--- adfs/certificatessetup.ps1
+++ adfs/certificatessetup.ps1
@@ -7,23 +7,12 @@ Configuration Certificates
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration, xStorage, xNetworking, xComputerManagement
 
-    #Step by step to reverse
-    #https://www.mssqltips.com/sqlservertip/4991/implement-a-sql-server-2016-availability-group-without-active-directory-part-1/
-    #
-    #2.1-Create a Windows Failover Cluster thru Powershell
-    #    https://docs.microsoft.com/en-us/sql/database-engine/availability-groups/windows/enable-and-disable-always-on-availability-groups-sql-server
-    #    https://docs.microsoft.com/en-us/powershell/module/failoverclusters/new-cluster?view=win10-ps
-    #    New-Cluster -Name “WSFCSQLCluster” -Node sqlao-vm1,sqlao-vm2 -AdministrativeAccessPoint DNS
-
     Node localhost
     {
-        # $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
-        # https://go.microsoft.com/fwlink/?LinkId=708343&clcid=0x409
-
         File DirectoryTemp
         {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Type = "Directory" # Default is "File".
+            Ensure = "Present"
+            Type = "Directory"
             Recurse = $false
             DestinationPath = "C:\TempDSCAssets"
             PsDscRunAsCredential = $AdminCreds
@@ -34,7 +23,7 @@ Configuration Certificates
             SetScript = 
             { 
                 $webClient = New-Object System.Net.WebClient 
-                $uri = New-Object System.Uri "https://lugizidscstorage.blob.core.windows.net/isos/adfs-certs.zip" 
+                $uri = New-Object System.Uri "https://raw.githubusercontent.com/mspnp/identity-reference-architectures/master/adfs/adfs-certs.zip" 
                 $webClient.DownloadFile($uri, "C:\TempDSCAssets\adfs-certs.zip") 
             } 
             TestScript = { Test-Path "C:\TempDSCAssets\adfs-certs.zip" } 
