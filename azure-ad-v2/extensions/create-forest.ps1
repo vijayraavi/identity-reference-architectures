d14191d446f9b6a33f333d6c47739de9dc510c2a
Pablo Russo <v-pabrus@microsoft.com>
2019-08-21 22:09:08 +0000

    Migrates onprem flow to azbb

---
+++ azure-ad-v2/extensions/create-forest.ps1
@@ -0,0 +1,122 @@
+﻿# $DomainName         -  FQDN for the Active Directory Domain to create
+# $DomainNetbiosName  -  AD domain netbios name
+# $AdminCreds         -  a PSCredentials object that contains username and password 
+#                        that will be assigned to the Domain Administrator account
+# $SafeModeAdminCreds -  a PSCredentials object that contains the password that will
+#                        be assigned to the Safe Mode Administrator account
+# $RetryCount         -  defines how many retries should be performed while waiting
+#                        for the domain to be provisioned
+# $RetryIntervalSec   -  defines the seconds between each retry to check if the 
+#                        domain has been provisioned 
+Configuration CreateForest {
+    param
+    #v1.4
+    (
+        [Parameter(Mandatory)]
+        [string]$DomainName,
+
+        [Parameter(Mandatory)]
+        [string]$DomainNetbiosName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SafeModeAdminCreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xStorage, xActiveDirectory, xNetworking, xPendingReboot
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($AdminCreds.UserName)", $AdminCreds.Password)
+    [System.Management.Automation.PSCredential ]$SafeDomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($SafeModeAdminCreds.UserName)", $SafeModeAdminCreds.Password)
+
+    $Interface = Get-NetAdapter|Where-Object Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+    
+    Node localhost
+    {
+        LocalConfigurationManager
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        } 
+
+        xWaitforDisk Disk2
+        {
+            DiskId = 2
+            RetryIntervalSec = 60
+            RetryCount = 20
+        }
+        
+        xDisk FVolume
+        {
+            DiskId = 2
+            DriveLetter = 'F'
+            FSLabel = 'Data'
+            FSFormat = 'NTFS'
+            DependsOn = '[xWaitForDisk]Disk2'
+        }        
+
+        WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"
+            IncludeAllSubFeature = $true
+        }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+            IncludeAllSubFeature = $true
+        }  
+
+        WindowsFeature RSAT
+        {
+             Ensure = "Present"
+             Name = "RSAT"
+        }        
+
+        xADDomain AddDomain
+        {
+            DomainName = $DomainName
+            DomainNetbiosName = $DomainNetbiosName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $SafeDomainCreds
+            ForestMode = "Win2012R2"
+            DomainMode = "Win2012R2"
+            DatabasePath = "F:\Adds\NTDS"
+            LogPath = "F:\Adds\NTDS"
+            SysvolPath = "F:\Adds\SYSVOL"
+            DependsOn = "[xWaitForDisk]Disk2","[WindowsFeature]ADDSInstall","[xDnsServerAddress]DnsServerAddress"
+        }
+
+        xWaitForADDomain DomainWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential = $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+            RebootRetryCount = 5
+            DependsOn = "[xADDomain]AddDomain"
+        }
+
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xWaitForADDomain]DomainWait"
+        }
+   }
+}
